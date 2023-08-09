813759bdaaefb31a7b34cce8c492a26fb5d67e20
Kevin Warui <waruimacharia@gmail.com>
2023-08-09 08:15:08 -0700

    Fix over-indentation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@
 for i in range(10):
     for j in range(10):
         if i != j and i < j:
-                if i == 8:
-                    print("{}{}".format(i, j))
-                else:
-                    print("{}{}".format(i, j), end=', ')
+            if i == 8:
+                print("{}{}".format(i, j))
+            else:
+                print("{}{}".format(i, j), end=', ')
