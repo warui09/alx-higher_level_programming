62ed44c9621431aed78bfbd98af60000ea7e5aff
Kevin Warui <waruimacharia@gmail.com>
2023-08-10 00:36:46 -0700

    Refactor 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-result = add(a, b)
+    result = add(a, b)
 
-print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, result))
