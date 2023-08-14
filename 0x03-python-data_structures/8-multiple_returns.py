ae3ab0daba3f903eff6c4b0fdb557a4dd94929b6
Kevin Warui <waruimacharia@gmail.com>
2023-08-14 02:51:20 -0700

    Add 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+
+    if len(sentence) > 0:
+        first = sentence[0]
+    else:
+        first = None
+
+    return(length, first)
