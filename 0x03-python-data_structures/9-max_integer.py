806c10fb07d4828bae4be35851743f0c79561b85
Kevin Warui <waruimacharia@gmail.com>
2023-08-14 03:54:59 -0700

    Refactor 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,4 +4,9 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        return max(my_list)
+        max_int = my_list[0]
+        for i in my_list:
+            if i > max_int:
+                max_int = i
+
+        return max_int
