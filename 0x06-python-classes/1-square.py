59bef019ce0c916c255f432b9c00bf16b20681ff
Kevin Warui <waruimacharia@gmail.com>
2023-08-29 01:58:34 -0700

    Add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Square:
+    """defines a square that has a private instance attribute
+    that is instantiated with size"""
+
+    def __init__(self, size):
+        self.__size = size
