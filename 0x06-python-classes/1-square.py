d83a27c13d9ee09076c91736c12aa2f3edb59146
Kevin Warui <waruimacharia@gmail.com>
2023-08-29 10:25:51 -0700

    Style 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """defines a square based on 0-square.py"""
 
+
 class Square:
     """defines a square that has a private instance attribute
     that is instantiated with size"""
