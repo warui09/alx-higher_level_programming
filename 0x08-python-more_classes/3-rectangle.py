0fd2ac405e01e66091a80c37a654d0122c420ab4
Kevin Warui <waruimacharia@gmail.com>
2023-09-04 09:51:10 -0700

    Add 3-rectangle.py

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""defines a rectangle by: (based on 2-rectangle.py)"""
+
+
+class Rectangle:
+    """Rectangle has two instance attributes height and
+    width and the associated getters and setters and
+    two public attributes area and perimeter
+    str() and str() print out the rectangle with #"""
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            result = ""
+            for i in range(self.__height):
+                result += "#" * self.__width
+                if i != self.__height - 1:
+                    result += "\n"
+            return result
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} object at {hex(id(self))}>"
