859a748f7229697be4da7df15c7c11b8aa7623d8
Kevin Warui <waruimacharia@gmail.com>
2023-09-05 10:05:55 -0700

    Add 5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""prints a text with 2 new lines after each of these characters:
+   ., ? and :"""
+def text_indentation(text):
+    """Attributes
+    text
+        must be a string, otherwise raise a TypeError with the
+        message "text must be a string"
+
+        There should be no space at the beginning or at the end
+        of each printed line
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for char in text:
+        print(char, end='')
+        if char in [".", "?", ":"]:
+            print("\n")
