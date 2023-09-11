973b8de41dd105762a06a6954706d0a0fcf9814c
Kevin Warui <waruimacharia@gmail.com>
2023-09-11 12:12:17 -0700

    Add 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""returns the list of available attributes and methods of an object"""
+def lookup(obj):
+    """Attributes
+    obj
+        object whose attributes and methods will be returned as a list
+    """
+    return dir(obj)
