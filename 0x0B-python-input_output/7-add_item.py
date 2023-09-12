bd4e5f1443473d8a53dbbb182482ee0924f7efb3
Kevin Warui <waruimacharia@gmail.com>
2023-09-12 13:10:48 -0700

    Add load from JSON to 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,15 @@
 
 import sys
 import json
+import os
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 new_list = []
+
+if os.path.exists("add_item.json"):
+    new_list = load_from_json_file("add_item.json")
 
 for i in range(1, len(sys.argv)):
     new_list.append(sys.argv[i])
