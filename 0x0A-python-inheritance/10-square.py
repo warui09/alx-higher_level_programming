7ffe06f249304b79e0fd060f7989a9150313c30f
Kevin Warui <waruimacharia@gmail.com>
2023-09-13 09:08:08 -0700

    Add 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Defines a class Square that inherits from Rectangle
+"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    Inherits from Rectangle
+
+    Attributes:
+        size: must be an integer
+    """
+
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
