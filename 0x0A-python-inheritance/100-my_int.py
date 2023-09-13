123dc86be053cc09bf73aa6b6ad5438845c87a47
Kevin Warui <waruimacharia@gmail.com>
2023-09-13 09:21:27 -0700

    Add 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Inherits from int
+"""
+class MyInt(int):
+    """
+    Inherits from int however == and != are swapped
+    """
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
