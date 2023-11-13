4d85c427d7c1c375d5e8ef754b466386985c7dc8
Kevin Warui <waruimacharia@gmail.com>
2023-11-13 04:03:05 -0800

    Edit 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,11 @@
 
 // prints the second biggest integer in the list of arguments
 
-const args = process.argv.slice(2).sort().reverse();
+const args = process.argv.slice(2).sort((a, b) => a - b).reverse();
+if (process.argv.length <= 3) {
+  console.log(0);
+}
+
 if (args[1]) {
   console.log(args[1]);
 }
-
-console.log(0);
