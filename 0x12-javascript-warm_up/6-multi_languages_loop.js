80078941bcc913d97774bc55e2590e06fee05d50
Kevin Warui <waruimacharia@gmail.com>
2023-11-13 01:32:33 -0800

    Add 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// prints 3 lines: (like 1-multi_languages.js)
+
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const i in text) {
+  console.log(text[i]);
+}
