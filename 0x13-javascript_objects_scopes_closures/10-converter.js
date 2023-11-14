4fdc833016bde22f29edbede6b301d9f8c46c9c2
Kevin Warui <waruimacharia@gmail.com>
2023-11-14 06:58:54 -0800

    Edit 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function toDecimal (num) {
-    return Math.floor(parseFloat(num));
-  }
-
-  function toHex (num) {
-    return num.toString(16);
-  }
-
-  switch (base) {
-    case 10:
-      return toDecimal;
-      break;
-    case 16:
-      return toHex;
-      break;
-    default:
-  }
+  return function (num) {
+    return num.toString(base);
+  };
 };
