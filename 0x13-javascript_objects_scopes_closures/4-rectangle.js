abae253c238d26ee88093dfebbbfca628c88a90e
Kevin Warui <waruimacharia@gmail.com>
2023-11-14 00:51:10 -0800

    Add 5-square.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,13 +15,13 @@ class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
 
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
