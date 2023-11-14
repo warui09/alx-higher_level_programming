7194bae86cd671d8de2de47a23f08322d55feaf0
Kevin Warui <waruimacharia@gmail.com>
2023-11-14 01:38:57 -0800

    Edit 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,11 +4,10 @@ const SquareParent = require('./5-square');
 class Square extends SquareParent {
   charPrint (c) {
     if (!c) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('C'.repeat(this.width));
-      }
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
