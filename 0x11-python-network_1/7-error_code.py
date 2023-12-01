e3f14f6c8ab06e4e3711e51a008348cca81b955d
Kevin Warui <waruimacharia@gmail.com>
2023-12-01 06:38:03 -0800

    Edit 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request to the URL and displays the body of the response."""
+"""Takes in a URL, sends a request to the URL and displays the body
+of the response."""
 
 import requests
 import sys
