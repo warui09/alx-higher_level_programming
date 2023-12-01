d9d3651a41746ad12d63d7f9ed7d60ef681beee2
Kevin Warui <waruimacharia@gmail.com>
2023-12-01 07:37:32 -0800

    Add 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    query_param = {"q": sys.argv[1]}
+    url = "http://0.0.0.0:5000/search_user"
+
+    res = requests.post(url, params=query_param)
+
+    if res.test:
+        try:
+            data = res.json()
+            if data:
+                print("[{}] {}".format(data.get("id"), data.get("name")))
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+    else:
+        print("No result")
