81dcd9464d37411062733f0dea3c60209bda0dd8
Kevin Warui <waruimacharia@gmail.com>
2024-01-04 02:31:55 -0800

    Edit 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-let fs = require('fs')
+const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-	if (!data)
-		console.log(err)
-	else
-		console.log(data)
+  if (!data) { console.log(err); } else { console.log(data); }
 }
-)
+);
