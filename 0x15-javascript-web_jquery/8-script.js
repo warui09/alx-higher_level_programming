fb8edb57024e35c471407dd249e94f78e281c706
Kevin Warui <waruimacharia@gmail.com>
2024-01-10 05:49:57 -0800

    100-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -2,6 +2,6 @@ $.when(
   $.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json'),
   $.ready
 ).done((data) => {
-  films = data[0].results;
+  const films = data[0].results;
   for (let i = 0; i < films.length; i++) { $('#list_movies').append(`<li>${films[i].title}</li>`); }
 });
